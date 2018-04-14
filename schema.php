91039480d136b08ac6dc103377b385e62f4472f3
Nat Taylor <nattaylor@gmail.com>
2018-04-13 23:18:11 -0400

    Added Autocomplete
    * Requires `schema.js` generated from `schema.php`
    * Requires table name to be set already

---
+++ schema.php
@@ -0,0 +1,43 @@
+<?php
+	$ch = curl_init("https://data.boston.gov/api/3/action/package_search?q=*:*&rows=150&");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$package_list = json_decode(curl_exec($ch))->result->results;
+	curl_close($ch);
+
+	$schema = array();
+
+	foreach ($package_list as $package) {
+		$ch = curl_init("https://data.boston.gov/api/3/action/datastore_search?limit=0&id=".$package->resources[0]->id);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);	
+		$result = json_decode(curl_exec($ch));
+		curl_close($ch);
+		if (!isset($result->result->fields)) {
+			continue;
+		}
+		$schema[$package->resources[0]->id] = array("fields" => array(), "title" => $package->title);
+		foreach($result->result->fields as $field) {
+			array_push($schema[$package->resources[0]->id]["fields"], $field->id);
+		}
+		//var_dump(count($schema));
+	}
+
+	echo "var schema = ".json_encode($schema, JSON_PRETTY_PRINT);
+
+/*
+[23]=>
+      object(stdClass)#143 (3) {
+        ["info"]=>
+        object(stdClass)#142 (3) {
+          ["notes"]=>
+          string(61) "Indicates the weekday associated with the daily metric values"
+          ["type_override"]=>
+          string(4) "text"
+          ["label"]=>
+          string(0) ""
+        }
+        ["type"]=>
+        string(4) "text"
+        ["id"]=>
+        string(16) "CTY_SCR_DAY_NAME"
+      }
+ */
